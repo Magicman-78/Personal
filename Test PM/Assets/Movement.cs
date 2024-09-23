de1a3143d68ed7a70d9832385c4cfc118d16d0ed
Josiah K. <1721887@mukilteo.wednet.edu>
2024-09-23 13:01:15 -0700

    Reworked movement and jump coding and added more notes.

--- Test PM/Assets/Movement.cs
+++ Test PM/Assets/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public KeyCode left = KeyCode.A, right = KeyCode.D, up = KeyCode.W, down = KeyCode.S, jump = KeyCode.W;
-    public float speed = 10, jumpHeight = 15;
+    public float speed = 10, jumpHeight = 15, maxSpeed = 10;
 
     private Rigidbody2D _rb;
     
@@ -25,31 +25,42 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(left)) // Check for the player HOLDING DOWN the left button
         {
             // Get the GameObject's RigidBody2D component set its velocity to the left at the given speed
-            _rb.velocity = Vector2.left * speed;
+            _rb.AddForce(Vector2.left * speed * Time.deltaTime);
+            // _rb.velocity = Vector2.left * speed;
         }
         
         if (Input.GetKey(right)) // Check for the player HOLDING DOWN the right button
         {
             // Get the GameObject's RigidBody2D component set its velocity to the right at the given speed
-            _rb.velocity = Vector2.right * speed;
+            _rb.AddForce(Vector2.right * speed * Time.deltaTime);
+            // _rb.velocity = Vector2.right * speed;
         }
 
         if (Input.GetKey(up)) // Check for the player HOLDING UP the right button
         {
             // Get the GameObject's RigidBody2D component set its velocity to the up at the given speed
-            _rb.velocity = Vector2.up * speed;
+            _rb.AddForce(Vector2.up * speed * Time.deltaTime);
+            //_rb.velocity = Vector2.up * speed;
         }
 
         if (Input.GetKey(down)) // Check for the player HOLDING DOWN the down button
         {
             // Get the GameObject's RigidBody2D component set its velocity to the down at the given speed
-            _rb.velocity = Vector2.down * speed;
+            _rb.AddForce(Vector2.down * speed * Time.deltaTime);
+            // _rb.velocity = Vector2.down * speed;
         }
+
+        if (_rb.velocity.x > maxSpeed)
+            _rb.velocity = new Vector2(maxSpeed, _rb.velocity.y);
+
+        if (_rb.velocity.x < -maxSpeed)
+            _rb.velocity = new Vector2(-maxSpeed, _rb.velocity.y);
 
         if (Input.GetKeyDown(jump))  //Check for the player PRESSING the jump button
         {
             //Get the GameObject's RigidBody2D component and set its velocity to the up at the given jump height
-            _rb.velocity = Vector2.down * jumpHeight;
+            _rb.AddForce(Vector2.down * jumpHeight, ForceMode2D.Impulse);
+            //_rb.Velocity = Vector2.up * jumpHeight;
         }
     }
 }
