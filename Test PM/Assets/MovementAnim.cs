ad18f7f7f67f99459f1dd7822a716d97df20df6b
Josiah K. <1721887@mukilteo.wednet.edu>
2024-09-30 13:32:20 -0700

    Almost completed animations and coding for it.

---
+++ Test PM/Assets/MovementAnim.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAnim : MonoBehaviour
+{
+    private Animator _anim;
+    private Rigidbody2D _rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rb2d.AddForce(Vector2.left * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rb2d.AddForce(Vector2.right * Time.deltaTime);
+        }
+
+        _anim.SetFloat("XSpeedAbs", _rb2d.velocity.x);
+    }
+}
